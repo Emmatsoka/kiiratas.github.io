f8d78a61566285530981756b26d8bef76ae64e5e
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-02-20 15:48:07 +0100

    .

---
+++ javascript.js
@@ -0,0 +1,38 @@
+document.write("‎ ")
+
+
+document.body.style.backgroundAttachment = "fixed";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.color = "black";
+document.body.style.fontFamily = "arial";
+
+
+
+
+function udvozol(){
+    var nev = document.progi.ide.value;
+    alert(" hello "+ nev);
+    document.progi.kiadat.value=("Üdv " + nev)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
